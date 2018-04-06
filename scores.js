6fa4b6651ad1ee6e45f1fa8b89d6ef40c6829bf8
Bob Hunt <bob.hunt.email@gmail.com>
2018-04-06 10:11:00 -0400

    commit

--- scores.js
+++ scores.js
@@ -1,34 +1,40 @@
+const jsonBody = require("body/json");
+var scores = [{ name: "Edwin", score: 50 }, { name: "David", score: 39 }];
+
+
 const textBody = require("body");
 const http = require('http');
 
-const jsonBody = require("body/json");
-var scores = [{name: "Edwin", score: 50}, {name: "David", score: 39}];
 
 const hostname = null;
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    var body;
-    if(req.method === "GET") {
-      if(resources[req.url] === undefined) {
-        res.statusCode = 404;
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        body = resources[req.url];
-      }
-    }else if(req.method === "PUT") {
-        res.statusCode = 201;
-        textBody(req, res, (err, body) => {
-          resources[req.url] = body;
-         })
-       
-    }
-     
-    res.end(body);
-  console.log(req.url);
-  console.log(req.headers);
-  console.log(req.method);
+  var body;
+
+  if (req.url === "undefined") {
+    res.statusCode = 404;
+  } else {
+    res.statusCode = 200;
+    // scores.sort();
+    // scores.reverse();
+    // scores.slice(0,3);
+
+    res.setHeader('Content-Type', 'application/javascript');
+    body = JSON.stringify(scores);
+  }
+
+  if (req.method === "GET") {
+  //
+  if (req.method === "POST") {
+  res.statusCode = 201;
+  scores.push(body);
+
+  //
+}
+res.end();
+console.log(req.url);
+console.log(req.method);
 });
 
 server.listen(port, hostname, () => {
